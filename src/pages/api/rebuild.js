4f48f9b557e15e6dfb6919cada358bbf38fd3801
Franklin Souza <frankrsouza@gmail.com>
2026-03-20 20:28:27 -0300

    ajuste no rebuild

--- src/pages/api/rebuild.js
+++ src/pages/api/rebuild.js
@@ -1,9 +1,20 @@
 // src/pages/api/rebuild.js
+let running = false;
 
-export async function GET() {
-  await fetch('https://api.vercel.com/v1/integrations/deploy/prj_ejZOlSeX0pLkQpqnR42Gk0wfBorY/evXK6jZUqF', {
-    method: 'POST'
-  });
+export async function GET({ request }) {
+  if (running) {
+    return new Response('Já em execução');
+  }
 
-  return new Response('Rebuild triggered');
+  running = true;
+
+  try {
+    await fetch('https://api.vercel.com/v1/integrations/deploy/prj_ejZOlSeX0pLkQpqnR42Gk0wfBorY/evXK6jZUqF', {
+      method: 'POST'
+    });
+  } finally {
+    running = false;
+  }
+
+  return new Response('OK');
 }
